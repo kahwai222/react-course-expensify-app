055780c5a956efd5037cb2719fe7a9e786019e9c
Kah Wai LIM <kwlim@ntu.edu.sg>
2019-07-26 22:57:17 +0800

    Store user data in user area

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -2,7 +2,7 @@ import * as firebase from 'firebase';
 
 const config = {
   apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  authDomain: "expensify-22b58.firebaseapp.com",
   databaseURL: process.env.FIREBASE_DATABASE_URL,
   projectId: process.env.FIREBASE_PROJECT_ID,
   storageBucket: '',
